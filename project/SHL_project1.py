89d0f7411923e9958e8c7c0c6f5b1c7710d633cc
minch99 <gfishone@snu.ac.kr>
2024-12-02 18:51:44 +0900

    SHL_v12

--- project/SHL_project1.py
+++ project/SHL_project1.py
@@ -93,7 +93,7 @@ class PurePursuit(Node):
             Also, Recommend set Ki extremely low.
         """
         #self.Kp = 1.32
-        self.Kp = 1.0
+        self.Kp = 1.3
         self.Ki = 0.005
         self.Kd = 0.05
         ###################################################
@@ -246,8 +246,8 @@ class PurePursuit(Node):
                         """
                         Save trajectory when terminated at 'args.traj_dir'.
                         """
-                        np.save(os.path.join(self.traj_dir, "obs_map5.npy"), np.array(obs_list))
-                        np.save(os.path.join(self.traj_dir, "act_map5.npy"), np.array(act_list))
+                        np.save(os.path.join(self.traj_dir, "obs_map_width4_0.npy"), np.array(obs_list))
+                        np.save(os.path.join(self.traj_dir, "act_map_width4_0.npy"), np.array(act_list))
                         ###################################################
                         ###################################################
                         self.get_logger().info(">>> map {} trajectory saved".format(map))
