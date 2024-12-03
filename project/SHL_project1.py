7893b4672afabcbd882972462a45618a62ef2c17
minch99 <gfishone@snu.ac.kr>
2024-12-04 08:20:12 +0900

    SHL_v50

--- project/SHL_project1.py
+++ project/SHL_project1.py
@@ -258,8 +258,8 @@ class PurePursuit(Node):
                         """
                         Save trajectory when terminated at 'args.traj_dir'.
                         """
-                        np.save(os.path.join(self.traj_dir, "obs_map18.npy"), np.array(obs_list))
-                        np.save(os.path.join(self.traj_dir, "act_map18.npy"), np.array(act_list))
+                        np.save(os.path.join(self.traj_dir, "obs_map19.npy"), np.array(obs_list))
+                        np.save(os.path.join(self.traj_dir, "act_map19.npy"), np.array(act_list))
                         ###################################################
                         ###################################################
                         self.get_logger().info(">>> map {} trajectory saved".format(map))
