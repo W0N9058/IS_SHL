1f04367abae0039693bbc954347d5e94a0ddb18b
minch99 <gfishone@snu.ac.kr>
2024-12-06 11:36:45 +0900

    SHL_v73

--- project/SHL_project1.py
+++ project/SHL_project1.py
@@ -80,7 +80,7 @@ class PurePursuit(Node):
         self.render = args.render
         self.time_limit = 180.0
         
-        self.lookahead = 25
+        self.lookahead = 5
 
         self.save = args.save
         self.traj_dir = args.traj_dir
