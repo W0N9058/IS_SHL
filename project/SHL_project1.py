d7e482695ee006309fa6a51bc6ad3f1f9eeddbf1
minch99 <gfishone@snu.ac.kr>
2024-12-06 12:10:28 +0900

    SHL_v75

--- project/SHL_project1.py
+++ project/SHL_project1.py
@@ -93,7 +93,7 @@ class PurePursuit(Node):
             Also, Recommend set Ki extremely low.
         """
         #self.Kp = 1.32
-        self.Kp = 1.0
+        self.Kp = 0.8
         self.Ki = 0.0
         self.Kd = 0.05
         ###################################################
